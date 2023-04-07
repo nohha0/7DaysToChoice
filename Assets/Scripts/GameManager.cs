758ea8730c51724dd6e6492f1cde0a6abe99a0d2
Shin004 <tktmdmswl004@gmail.com>
2023-04-07 13:34:54 +0900

    add GameManager
    add TIme

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -23,6 +23,11 @@ public class GameManager : MonoBehaviour
     void TimeUpdate() //시간 업데이트
     {
 
+        if(Time_Hour <=24)
+        {
+            Time_Hour = Time_Hour - 24;
+        }
+
         if (Time_minute >= 60) //시간 증가
         {
             int over = Time_minute - 60;
@@ -33,7 +38,19 @@ public class GameManager : MonoBehaviour
 
     void TodayOff()  //하루 끝내기
     {
-        
+        //하루 종료되면 이벤트 전개
+        Time_Hour = 8;
+        Time_minute = 0;
+    }
+    public void addTime( int Hour, int min)  //시간추가
+    {
+        Time_Hour += Hour;
+        Time_minute += min;
+
     }
         
+    public void addDay()  //일차추가
+    {
+        Day++;
+    }
 }
