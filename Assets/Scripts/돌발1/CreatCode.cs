82797bda9eba3da36aa219d8c43a3cf8415de5ff
Shin004 <115400628+Shin004@users.noreply.github.com>
2023-04-11 19:29:47 +0900

    add Minigame
    UI형태는 갖춤
    플레이 안됨

---
+++ Assets/Scripts/돌발1/CreatCode.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatCode : MonoBehaviour
+{
+    public GameObject Greencode; //작업 활성화
+    public GameObject Redcode;   //작업 에러
+    public GameObject Blackcode; //작업 침체
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
