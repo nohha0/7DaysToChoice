3b22db81f8f17bdb3f72a52ba047d13ea8f4cc13
nohha0 <114228512+nohha0@users.noreply.github.com>
2023-04-12 11:35:56 +0900

    merge test 1
    돌발 scene

--- Assets/Scripts/돌발1/CreatCode.cs
+++ Assets/Scripts/돌발1/CreatCode.cs
@@ -7,6 +7,7 @@ public class CreatCode : MonoBehaviour
     public GameObject Greencode; //작업 활성화
     public GameObject Redcode;   //작업 에러
     public GameObject Blackcode; //작업 침체
+    int a;
     void Start()
     {
 
